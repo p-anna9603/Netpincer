965fd5b837bdd8eb47f083901b9b05d2bda93575
Anna Pauló <p.anna9603@gmail.com>
2021-03-28 18:29:24 +0200

    Client window done - this time it is not deleted fc yeah

---
+++ RestaurantClient/UserMain.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantClient
+{
+    /// <summary>
+    /// Interaction logic for UserMain.xaml
+    /// </summary>
+    public partial class UserMain : Window
+    {
+        UserRestaurantList restaurants;
+        public UserControl child;
+        private ConnectToServer serverConnection;
+        User currUser;
+
+        public UserMain(ConnectToServer ServerCon, User usr)
+        {
+            InitializeComponent();
+            serverConnection = ServerCon;
+            currUser = usr;
+        }
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contacts.Visibility = Visibility.Collapsed;
+                tt_messages.Visibility = Visibility.Collapsed;
+                tt_maps.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contacts.Visibility = Visibility.Visible;
+                tt_messages.Visibility = Visibility.Visible;
+                tt_maps.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_signout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e) // menü összecsukása
+        {
+            img_bg.Opacity = 1;
+            Console.WriteLine("tgbutn unchecked");
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e) // menü megnyitása
+        {
+            img_bg.Opacity = 0.3;
+            Console.WriteLine("tgbutn checked");
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = LV.SelectedIndex;
+            //      MoveCursorMenu(index);
+            Console.WriteLine("listview menu" + index);
+            switch (index)
+            {
+                case 0:
+                    //GridPrincipal.Children.Clear();
+                    childWindow.Content = null;
+                    Console.WriteLine("0 clear");
+                    break;
+                case 1:
+                    //GridPrincipal.Children.Clear();
+                    Console.WriteLine("1 clear");
+                    childWindow.Content = null;
+                    restaurants = new UserRestaurantList(this);
+                    //GridPrincipal.Children.Add(menus);
+                    //menus.Width = GridPrincipal.Width;
+                    //menus.Height = GridPrincipal.Height;
+
+                    childWindow.Content = restaurants;
+
+                    child = restaurants;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void window_sizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (child != null)
+            {
+                //child.Width = childWindow.Width;
+                //child.Height = childWindow.Height;
+                //Console.WriteLine("child. " + child.Width + ", " + child.Height);
+                //Console.WriteLine("GridPrincipal. " + childWindow.Width + ", " + childWindow.Height);
+            }
+        }
+
+        private void childWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ContentControl window = sender as ContentControl;
+
+            if (child != null)
+            {
+                child.Width = window.Width;
+                child.Height = window.Height - 50;
+                Console.WriteLine("child. " + child.Width + ", " + child.Height);
+                Console.WriteLine("GridPrincipal. " + e.NewSize.Width + ", " + e.NewSize.Height);
+            }
+        }
+        public ConnectToServer ServerConnection { get => serverConnection; set => serverConnection = value; }
+    }
+}
---
+++ RestaurantClient/UserRestaurantList.xaml.cs
@@ -0,0 +1,123 @@
+﻿using FoodOrderClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantClient
+{
+    /// <summary>
+    /// Interaction logic for UserRestaurantList.xaml
+    /// </summary>
+    public partial class UserRestaurantList : UserControl
+    {
+        public ConnectToServer ServerConnection;
+        UserMain userMain;
+        List<Restaurant> restList;
+        Dictionary<StackPanel, int> restPanels = new Dictionary<StackPanel, int>();
+
+        public UserRestaurantList(Window parent)
+        {
+            InitializeComponent();
+            userMain = (UserMain)parent;
+            ServerConnection = userMain.ServerConnection;
+            // restList = szerver lista
+            addExistingRestaurants();
+        }
+
+        public void addExistingRestaurants()
+        {
+            for(int i = 0; i < restList.Count; ++i)
+            {               
+                addCategoryPanel(restList[i].name, restList[i].restaurantID);
+            }
+        }
+        private void addCategoryPanel(string restName, int restID)
+        {
+            Console.WriteLine("addcateg");
+            StackPanel restaurantPanel = new StackPanel();
+            restaurantPanel.Orientation = Orientation.Vertical;
+            restaurantPanel.HorizontalAlignment = HorizontalAlignment.Center;
+            restaurantPanel.Width = 200;
+            restaurantPanel.Height = 132;
+            SolidColorBrush color = new SolidColorBrush();
+            color.Color = Color.FromArgb(50, 255, 204, 153);
+            restaurantPanel.Background = color;
+            restaurantPanel.Margin = new Thickness(5, 5, 0, 0);
+
+            Border border = new Border();
+            border.BorderBrush = new SolidColorBrush(Colors.AliceBlue);
+            border.BorderThickness = new Thickness(1);
+
+            TextBlock newText = new TextBlock();
+            newText.Name = "categName";
+            newText.VerticalAlignment = VerticalAlignment.Top;
+            newText.HorizontalAlignment = HorizontalAlignment.Center;
+            newText.TextAlignment = TextAlignment.Center;
+            newText.Width = 200;
+            newText.Height = 50;
+            newText.FontFamily = new FontFamily("Century");
+            newText.FontSize = 16;
+            newText.TextWrapping = TextWrapping.Wrap;
+            newText.Padding = new Thickness(2, 10, 2, 0);
+            newText.Text = restName;
+
+            StackPanel panel2 = new StackPanel();
+            panel2.Orientation = Orientation.Horizontal;
+            panel2.Height = 80;
+
+            /* Category image */
+            Image img = new Image();
+            //img.Source = new BitmapImage(new Uri("pack://application:,,,/RestaurantClient;component/Assets/img_setting.png")); // or
+            img.Source = new BitmapImage(new Uri("Assets/menu.png", UriKind.Relative)); // TODO pic from db
+            img.Stretch = Stretch.Fill;
+            img.Margin = new Thickness(30, 8, 11, 0);
+            img.Height = 70;
+            img.Width = 138;
+            img.HorizontalAlignment = HorizontalAlignment.Center;
+            img.VerticalAlignment = VerticalAlignment.Center;
+        //    imgNames[categID] = img;
+
+            panel2.Children.Add(img);
+            border.Child = newText;
+
+            restaurantPanel.MouseDown += restImg_MouseDown;
+            restaurantPanel.Children.Add(border);
+            restaurantPanel.Children.Add(panel2);
+            //    restaurantPanel.Children.Add(img);
+            restaurantPanel.MouseEnter += restaurant_MouseEnter;
+            restaurantPanel.MouseLeave += restaurant_MouseLeave;
+            restPanels[restaurantPanel] = restID;
+            menuList.Children.Add(restaurantPanel);
+        }
+        private void restaurant_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StackPanel panel = sender as StackPanel;
+            SolidColorBrush color = new SolidColorBrush();
+            color.Color = Color.FromArgb(50, 255, 204, 153);
+            panel.Background = color;
+        }
+
+        private void restaurant_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StackPanel panel = sender as StackPanel;
+            SolidColorBrush color = new SolidColorBrush();
+            color.Color = Color.FromArgb(100, 255, 204, 153);
+            panel.Background = color;
+        }
+        private void restImg_MouseDown(object sender, RoutedEventArgs e)    // Get restaurant foods?
+        {
+
+        }
+    }
+}
