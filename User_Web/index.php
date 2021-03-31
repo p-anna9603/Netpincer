bd5579a262f2b8256e282310dce96316d30ee15d
Marcelion1999 <marcelion1999@gmail.com>
2021-03-31 21:26:53 +0200

    still nem mukodik

--- User_Web/index.php
+++ User_Web/index.php
@@ -41,14 +41,141 @@
       </div>
     </nav>
     <!-- Navbar -->
+    <div class="px-4 pt-5 my-5 text-center border-bottom">
+  <h1 class="display-4 fw-bold">Netpincér</h1>
+  <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4"><i>"Ahol az étel házhoz jön"</i></p>
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+      <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Lorem</button>
+      <button type="button" class="btn btn-outline-secondary btn-lg px-4">Ipsum</button>
+    </div>
+  </div>
+  <div class="overflow-hidden" style="max-height: 30vh;">
+    <div class="container px-5">
+      <img src="bootstrap-docs.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+    </div>
+  </div>
+</div>
+    <!-- Main-->
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">Vásárló</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, explicabo autem libero alias asperiores eius? Est odio soluta labore sit, dolores omnis sapiente in hic tempore quod incidunt expedita!</p>
+              <a href="#" class="btn btn-primary">Kezdj vásárolni</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body ">
+              <h5 class="card-title text-center" >Futár</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iusto molestias sapiente consectetur. Fuga aut, delectus ducimus dolore optio assumenda vero velit ex. Nulla repellat velit accusantium doloremque, non sunt!</p>
+              <a href="#" class="btn btn-primary">Kezdj Dolgozni</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">Étterem</h5>
+              <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic adipisci eligendi quas doloribus laudantium suscipit aut sequi sit molestiae amet deleniti provident rerum fugit, veniam, omnis at quis atque error!.</p>
+              <a href="#" class="btn btn-primary btn-lg btn-block">Hirdesd meg</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+  
+<!-- Main -->
 
 
   <div class="container">
-    <?php 
-      include("src/connect_server.php");
-    ?>
+  <div class="card">
+            <p class="lead mb-4">Tesztelődoboz</p>
+            <?php 
+              include("src/connect_server.php");
+            ?>
+            </div>
+    
   </div>
+
+  <!-- Footer -->
+<footer class="bg-light text-center text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Készítették</h5>
+
+        <p>
+          Pauló Anna <br>
+          Bokor Klaudia<br>
+          Soós Péter<br>
+          Szabó Marcell
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Lorem</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-dark">Ipsum</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">dolor</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">sit</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">amet</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-dark">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
--- User_Web/src/connect_server.php
+++ User_Web/src/connect_server.php
@@ -1,23 +1,76 @@
 <?php 
+
+error_reporting(E_ALL);
+
+    /* Get the port for the WWW service. */
+    $service_port = 11000;
+
+    /* Get the IP address for the target host. */
+    $address = gethostbyname('localhost');
+
+    /* Create a TCP/IP socket. */
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if ($socket === false) {
+        echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+    } else {
+        echo "OK.<br>";
+    }
+
+    echo "Attempting to connect to '$address' on port '$service_port'...";
+    $result = socket_connect($socket, $address, $service_port);
+    if ($result === false) {
+        echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+    } else {
+        echo "OK.<br>";
+    }
+
+$in="1";
+
+echo "Sending request...";
+socket_write($socket, $in, strlen($in));
+echo "OK.<br>";
+
+
+echo "Reading response:\n\n";
+while ($out = socket_read($socket, 2048)) {
+    echo $out;
+}
+
+    echo "Closing socket...";
+    socket_close($socket);
+    echo "OK.\n\n";
+
 /*
-if(!($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)))
+
+$ip = "127.0.0.1";
+$port = "1433";
+$sendData = "pls mukodj";
+
+if(!($sock = socket_create(AF_INET, SOCK_STREAM,0)))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
      
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
+else
+{
+    echo "Socket created <br>";
+}
  
-echo "Socket created";
-if(!socket_connect($sock , '127.0.0.1' , 1433))
+
+if(!socket_connect($sock , $ip, 0))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
      
     die("Could not connect: [$errorcode] $errormsg \n");
 }
- 
-echo "Connection established \n";
+else
+{
+    echo "Connection established <br>";
+}
+
 
 
 $message = "GET / HTTP/1.1\r\n\r\n";
@@ -44,15 +97,13 @@ if(socket_recv ( $sock , $buf , 80 , MSG_WAITALL ) === FALSE)
 }
  
 //print the received message
-echo $buf;*/
+echo $buf;
 
-$ip = "127.0.0.1";
-$port = "1433";
-$sendData = "pls mukodj";
+
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-socket_connect($socket,$ip, $port);
+//socket_connect($socket,$ip, $port);
 
-socket_write($socket, "1");
+socket_write($socket, "1");*/
 ?>
